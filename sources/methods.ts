407c0f4f96af5f26c6e4d3c4ef04d12ef743641a
Javier R <javier.rios-urbano@epitech.eu>
2023-07-08 13:48:09 +0200

    add: test more methods

--- sources/methods.ts
+++ sources/methods.ts
@@ -1,8 +1,8 @@
-import { type ContractEventName, ethers } from "https://esm.sh/ethers@6.6.2";
+import { type ContractEventName, ethers } from "ethers";
 import { filterAbi, getAbi, handler } from "./utils.ts";
 import { AbiType } from "./types.ts";
 
-const provider = new ethers.JsonRpcProvider("https://erpc.apothem.network/");
+const provider = new ethers.JsonRpcProvider("https://erpc.xinfin.network/");
 
 export const getBlockNumber = async () => await provider.getBlockNumber();
 
@@ -57,28 +57,34 @@ export const getPastEvents = async (
     provider,
   );
   const pastEventPromises = events.map(async (eventName) => {
-    const fromBlock = (await provider.getBlockNumber()) - (days * 86400) / 2;
+    const fromBlock = (await provider.getBlockNumber()) - (days * 86400) / 15;
     const toBlock = await provider.getBlockNumber();
 
     return await contract.queryFilter(eventName, fromBlock, toBlock);
   });
 
-  return (await Promise.all(pastEventPromises)).flatMap(([p]) => p);
+  return ((await Promise.all(pastEventPromises)).flatMap((k) => k));
 };
 
 if (import.meta.main) {
   // TODO(jabolo): remove this, used to check testnet events
   // await follow("xdc8dA38026f5bB57D20485299903858092AD9C9fBA", ["Transfer"]);
-  console.log(
-    await getPastEvents(
-      // "xdcc5acf77c1f2bdbbaa75a6331a351bee006543616",
-      "xdc8dA38026f5bB57D20485299903858092AD9C9fBA",
-      // AbiType.XRC_721,
-      AbiType.XRC_20,
-      [
-        "Transfer",
-      ],
-      365 * 2,
-    ),
+
+  const type: AbiType = AbiType.XRC_721;
+  const address = "xdc85d216d87C993c250A7725aF8f6C161d0504c32B";
+  const days = 365;
+  const events = [["Transfer", "Approval"], [
+    "Transfer",
+    "Approval",
+    "ApprovalForAll",
+  ]][type];
+
+  const data = await getPastEvents(address, type, events, days);
+
+  console.log(data.length);
+
+  await Deno.writeFile(
+    `out_${["20", "721"][type]}.json`,
+    new TextEncoder().encode(JSON.stringify(data, null, 2)),
   );
 }
